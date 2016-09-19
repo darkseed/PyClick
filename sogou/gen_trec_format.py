7432a34b68629b642ca26c4e0bafe4bf23ec1695
AdeDZY <zhuyund@cs.cmu.edu>
2016-09-19 14:21:38 -0400

    bos

--- sogou/gen_trec_format.py
+++ sogou/gen_trec_format.py
@@ -1,11 +1,12 @@
 import argparse
 import json
+import codecs
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("query_file", type=argparse.FileType('r'))
+    parser.add_argument("query_file")
     parser.add_argument("click_model_file")
-    parser.add_argument("url_title_segmented_file", type=argparse.FileType('r'))
+    parser.add_argument("url_title_segmented_file")
     parser.add_argument("output_file", type=argparse.FileType('w'))
     args = parser.parse_args()
 
@@ -13,19 +14,22 @@ if __name__ == '__main__':
     query2qid = {}
     qid2segmented = {}
     qid = 1
-    for line in args.query_file:
+    for line in codecs.open(args.query_file, 'r', 'utf-8'):
+        line = line.encode("utf-8")
         tf, query_segmented = line.strip().split('\t')
-        query = ''.join([query_segmented.split(' ')])  # segmented query to original query
+        query = ''.join(query_segmented.split(' '))  # segmented query to original query
         query2qid[query] = qid
-        qid2segmented[query] = query_segmented
+        qid2segmented[qid] = query_segmented
         qid += 1
+        print query
 
     # read click model output first time, gather urls
     url2uid = {}
     uid2title = {}
     uid = 1
-    with open(args.click_model_file) as f:
+    with codecs.open(args.click_model_file, 'r', 'utf-8') as f:
         for line in f:
+            line = line.encode("utf-8")
             query, url, rel_score = line.strip().split(' ')
             if query not in query2qid:
                 continue
@@ -33,10 +37,13 @@ if __name__ == '__main__':
             if url not in url2uid:
                 url2uid[url] = uid
                 uid += 1
-
     # read url title segmented result
-    for line in args.url_title_segmented_file:
-        url, segmented = line.strip().split('\t')
+    for line in codecs.open(args.url_title_segmented_file, 'r', 'utf-8'):
+        line = line.encode("utf-8")
+        line = line.strip()
+        if not line or '\t' not in line:
+            continue 
+        url, segmented = line.split('\t')
         if url not in url2uid:
             continue
         uid = url2uid[url]
@@ -45,13 +52,16 @@ if __name__ == '__main__':
     # read click model output second time, output trec format
     prev_qid = -1
     rank = 1
-    with open(args.click_model_file) as f:
+    with codecs.open(args.click_model_file, 'r', 'utf-8', errors="replace") as f:
         for line in f:
+            line = line.encode("utf-8")
             query, url, rel_score = line.strip().split(' ')
             if query not in query2qid:
                 continue
             url = url.strip()
             uid = url2uid[url]
+            if uid not in uid2title or url == "http://weixin.qq.com/":
+                continue
             qid = query2qid[query]
             if qid != prev_qid:
                 prev_qid = qid
@@ -59,6 +69,6 @@ if __name__ == '__main__':
             else:
                 rank += 1
             score = -rank
-            jstr = json.dumps({"query": qid2segmented[qid], "doc": {"title": url + ' ' + uid2title[uid]}})
+            jstr = json.dumps({"query": qid2segmented[qid], "doc": {"title": url + ' ' + uid2title[uid]}}, ensure_ascii=False)#.encode('utf-8') 
             args.output_file.write("{0}\tQ0\tsogou-test-{1}\t{2}\t{3} # {4}\n".format(qid, uid, rank, score, jstr))
 
